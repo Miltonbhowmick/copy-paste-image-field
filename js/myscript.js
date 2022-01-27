a124d0e18d30d12663aaaa3bc2ca39e9ade8b657
Miltonbhowmick <miltonbhowmick6@gmail.com>
2022-01-27 21:13:47 +0600

    updated the js with paste event of js

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,16 @@
 
-function ck() {
-    console.log("hi");       
+// on paste event function
+document.onpaste = function(event){
+	var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+	for (var index in items) {
+        var item = items[index];
+        if (item.kind === 'file') {
+            var blob = item.getAsFile();
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                console.log(event.target.result);
+            }; 
+            reader.readAsDataURL(blob);
+        }
+    }
 }
